a391bfbb5dae49ea2ef60d71ddd7cc7f3e065727
Yago Batista <yagobatistafis@yahoo.com.br>
2022-06-09 23:04:15 -0300

    updating data structure

--- constans/Constants.js
+++ constans/Constants.js
@@ -2,29 +2,46 @@ import worldCup from "../public/worldCupSimulator.png";
 import countryCodex from "../public/countrycodex.png";
 import jokenpo from "../public/jokenpo.png";
 import crypto from "../public/crypto.png";
+
 export const projects = [
   {
     title: "World Cup Simulator",
     demo: "https://world-cup-simulator.vercel.app",
     github: "https://github.com/YagoLeite/World-Cup-Simulator",
     img: worldCup,
+    textOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgOne: "image one",
+    textTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgTwo: "image two",
   },
   {
     title: "Jokenpo",
     demo: "https://jokenpo-inky.vercel.app",
     github: "https://github.com/YagoLeite/jokenpo",
     img: jokenpo,
+    textOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgOne: "image one",
+    textTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgTwo: "image two",
   },
   {
     title: "Country Codex",
     demo: "https://country-codex.vercel.app",
     github: "https://github.com/YagoLeite/where-in-the-world",
     img: countryCodex,
+    textOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgOne: "image one",
+    textTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgTwo: "image two",
   },
   {
-    title: "World Cup Simulator",
+    title: "Crypto",
     demo: "https://crypto-tracker-3e62e.web.app",
     github: "https://github.com/YagoLeite/crypto-tracker",
     img: crypto,
+    textOne: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgOne: "image one",
+    textTwo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+    imgTwo: "image two",
   },
 ];
